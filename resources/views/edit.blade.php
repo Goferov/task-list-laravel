9d8c44107413aa37a4072cccb6870b477ce73771
Goferov <marcingodfryd@gmail.com>
2023-10-14 23:23:43 +0200

    Refactor add/edit task forms

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,43 +1,4 @@
 @extends('layouts.app')
-
-@section('title', 'Edit task')
-
-@section('style')
-    <style>
-        .error-msg {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-    <form method="post" action="{{ route('tasks.update', ['task' => $task->id]) }}">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            <input name="title" id="title" value="{{ $task->title }}">
-            @error('title')
-            <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
-            @error('description')
-            <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="long_description">Long description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
-            @error('long_description')
-            <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <button type="submit">Edit task</button>
-        </div>
-    </form>
+    @include('form', ['task' => $task])
 @endsection
